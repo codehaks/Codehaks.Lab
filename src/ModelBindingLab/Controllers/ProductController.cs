5285378e8ac8feed5ae9c6fa391add0de963ee44
Hakim Ghods <ghods.hakim@hotmail.com>
2018-11-13 16:06:49 +0330

    Add project files.

---
+++ src/ModelBindingLab/Controllers/ProductController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingLab.Models;
+
+namespace ModelBindingLab.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+ 
+        public IActionResult Create(string name,int price)
+        {
+            // Save to database
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View("Details");
+        }
+        public IActionResult Create1(string name, int price)
+        {
+            // Save to database
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View("Details");
+        }
+        public IActionResult Create2(Product model)
+        {
+            // Save to database
+            ViewData["name"] = model.Name;
+            ViewData["price"] = model.Price;
+            return View("Details");
+        }
+
+        [Route("/Create/List")]
+        [HttpGet]
+        public IActionResult CreateList()
+        {
+        
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Create/List")]
+        public IActionResult CreateList(IList<Product> model)
+        {
+
+            return View("List", model);
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upload([FromServices] IHostingEnvironment _env,  List<IFormFile> files)
+        {
+            var path = _env.ContentRootPath + @"\files\";
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = path + formFile.FileName;
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+    }
+}
---
+++ src/ModelBindingLab/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingLab.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
